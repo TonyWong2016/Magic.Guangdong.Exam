40c577252d06b20a060ee6ec6c0c251f3a22ba11
Tony_df <wtlemon@126.com>
2024-10-22 17:45:24 +0800

    增加配置页面

--- Magic.Guangdong.Exam.Client/Filters/GlobalActionFilter.cs
+++ Magic.Guangdong.Exam.Client/Filters/GlobalActionFilter.cs
@@ -37,119 +37,133 @@ namespace Magic.Guangdong.Exam.Client.Filters
         /// <param name="context"></param>
         public async void OnActionExecuting(ActionExecutingContext context)
         {
-            Console.WriteLine("start:"+DateTime.Now.ToString());
-            if (context.RouteData == null ||
-                context.RouteData.Values == null ||
-                context.HttpContext.Request.Path==null
-                )
+            try
             {
-                context.Result = new JsonResult(new { code = -1, msg = "请求地址错误" });
-                return;
-            }
-            string accountId = Guid.Empty.ToString();
-            string requestMethod = context.HttpContext.Request.Method.ToLower();
-            if (context.HttpContext.Request.Cookies.Where(u => u.Key == "accountId").Any())
-            {
-                accountId = Utils.FromBase64Str(context.HttpContext.Request.Cookies["accountId"]);
+                Console.WriteLine("start:" + DateTime.Now.ToString());
+                if (context.RouteData == null ||
+                    context.RouteData.Values == null ||
+                    context.HttpContext.Request.Path == null
+                    )
+                {
+                    context.Result = new JsonResult(new { code = -1, msg = "请求地址错误" });
+                    return;
+                }
+                string accountId = Guid.Empty.ToString();
+                string requestMethod = context.HttpContext.Request.Method.ToLower();
+                if (context.HttpContext.Request.Cookies.Where(u => u.Key == "accountId").Any())
+                {
+                    accountId = Utils.FromBase64Str(context.HttpContext.Request.Cookies["accountId"]);
 
-            }
+                }
 
-            string router = context.HttpContext.Request.Path.Value;
-            //执行方法前先执行这
-            var actionLog = $"{DateTime.Now} 开始调用 【{router}】 api；参数为：{Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments)}";
-            
-            var descriptor = context.ActionDescriptor as Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor;
-            if (descriptor == null)
-            {
-                return;
-            }
-            //标记为无需验证的操作直接跳过
-            if (descriptor.MethodInfo.CustomAttributes.Any(u => u.AttributeType.Name == "AllowAnonymousAttribute"))
-            {
-                actionLog += ",（当前接口为开放类型）";
-                Logger.Info(actionLog);
-                return;
-            }
-            if (descriptor.MethodInfo.CustomAttributes.Any(u => u.AttributeType.Name == "RouteMark"))
-            {
-                actionLog = $"用户【{accountId}】，于{DateTime.Now} 开始调用 【{router}】 api；参数为：{Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments)}";
+                string router = context.HttpContext.Request.Path.Value;
+                //执行方法前先执行这
+                var actionLog = $"{DateTime.Now} 开始调用 【{router}】 api；参数为：{Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments)}";
 
-                Logger.Warning(actionLog);
-            }
-            else
-                Logger.Info(actionLog);
+                var descriptor = context.ActionDescriptor as Microsoft.AspNetCore.Mvc.Controllers.ControllerActionDescriptor;
+                if (descriptor == null)
+                {
+                    return;
+                }
+                //标记为无需验证的操作直接跳过
+                if (descriptor.MethodInfo.CustomAttributes.Any(u => u.AttributeType.Name == "AllowAnonymousAttribute"))
+                {
+                    actionLog += ",（当前接口为开放类型）";
+                    Logger.Info(actionLog);
+                    return;
+                }
+                if (descriptor.MethodInfo.CustomAttributes.Any(u => u.AttributeType.Name == "RouteMark"))
+                {
+                    actionLog = $"用户【{accountId}】，于{DateTime.Now} 开始调用 【{router}】 api；参数为：{Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments)}";
 
-            var routeMarks = descriptor.MethodInfo.CustomAttributes.Where(u => u.AttributeType.Name.Contains("RouteMark"));
-            string actionMark = "";
-            if (routeMarks != null && routeMarks.Any() && routeMarks.First().ConstructorArguments.Any())
-            {
-                actionMark = routeMarks.First().ConstructorArguments.First().Value.ToString();
+                    Logger.Warning(actionLog);
+                }
+                else
+                    Logger.Info(actionLog);
+
+                var routeMarks = descriptor.MethodInfo.CustomAttributes.Where(u => u.AttributeType.Name.Contains("RouteMark"));
+                string actionMark = "";
+                if (routeMarks != null && routeMarks.Any() && routeMarks.First().ConstructorArguments.Any())
+                {
+                    actionMark = routeMarks.First().ConstructorArguments.First().Value.ToString();
+                }
+                if (requestMethod != "get")
+                {
+                    //await _capPublisher.PublishAsync(CapConsts.PREFIX + "AddKeyAction", new KeyAction()
+                    //{
+                    //    AdminId = Guid.Empty,
+                    //    AccountId = Guid.Parse(accountId),
+                    //    Action = actionMark,
+                    //    Description = actionLog.Length > 1800 ? actionLog.Substring(0, 1800) : actionLog,
+                    //    Router = router,
+                    //    CreatedAt = DateTime.Now,
+                    //    Type = 2
+                    //}) ;
+                }
+                RequestLog(context);
+                Console.WriteLine("end:" + DateTime.Now.ToString());
             }
-            if (requestMethod != "get")
-            {
-                //await _capPublisher.PublishAsync(CapConsts.PREFIX + "AddKeyAction", new KeyAction()
-                //{
-                //    AdminId = Guid.Empty,
-                //    AccountId = Guid.Parse(accountId),
-                //    Action = actionMark,
-                //    Description = actionLog.Length > 1800 ? actionLog.Substring(0, 1800) : actionLog,
-                //    Router = router,
-                //    CreatedAt = DateTime.Now,
-                //    Type = 2
-                //}) ;
+            catch (Exception ex) {
+                Console.WriteLine("响应日志记录异常" + DateTime.Now.ToString() + ex.Message);
             }
-            RequestLog(context);
-            Console.WriteLine("end:" + DateTime.Now.ToString());
         }
 
         private void RequestLog(ActionExecutingContext context)
         {
-            string ip = "";
-            if (context.HttpContext.Request.Headers["x-Forwarded-For"].Any())
+            try
             {
-                ip = context.HttpContext.Request.Headers["x-Forwarded-For"].FirstOrDefault();
+                string ip = "";
+                if (context.HttpContext.Request.Headers["x-Forwarded-For"].Any())
+                {
+                    ip = context.HttpContext.Request.Headers["x-Forwarded-For"].FirstOrDefault();
+                }
+                else if (context.HttpContext.Request.Headers["X-Real-IP"].Any())
+                {
+                    ip = context.HttpContext.Request.Headers["X-Real-IP"].FirstOrDefault();
+                }
+                else if (context.HttpContext.Request.Headers["HTTP_X_FORWARDED_FOR"].Any())
+                {
+                    ip = context.HttpContext.Request.Headers["HTTP_X_FORWARDED_FOR"].FirstOrDefault();
+                }
+                else
+                {
+                    ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
+                }
+                string user = Guid.Empty.ToString();
+                //if (context.HttpContext.User.Claims.Any())
+                //{
+                //    user = context.HttpContext.User.Claims.First().Value;
+                //}
+                if (context.HttpContext.Request.Cookies.Where(u => u.Key == "userId").Any())
+                {
+                    user = Utils.FromBase64Str(context.HttpContext.Request.Cookies["userId"]);
+                }
+                string method = context.HttpContext.Request.Method;
+                string url = context.HttpContext.Request.Path.Value;
+                string param = Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments);
+                string remark = context.HttpContext.Request.Headers["User-Agent"];
+
+                string brower = "UnKnown";
+                if (remark.Contains("FireFox"))
+                    brower = "FireFox";
+                else if (remark.Contains("Chrome"))
+                    brower = "Chrome";
+                else if (remark.Contains("Safari"))
+                    brower = "Safari";
+                string logMode = ConfigurationHelper.GetSectionValue("LogMode");
+                if (logMode == "es")
+                    Task.Run(() => Logger.writeLogToRedis($"{DateTime.Now.ToString("HH:mm:ss")} {user} {method} {url} \"{param}\" {ip} {brower} \"{remark}\"", "info"));
+                else
+                {
+                    string msg = $"{DateTime.Now.ToString("HH:mm:ss")} {user} {method} {url} \"{param}\" {ip} {brower} \"{remark}\"";
+                    Logger.Verbose(msg);
+                }
             }
-            else if (context.HttpContext.Request.Headers["X-Real-IP"].Any())
+            catch (Exception ex)
             {
-                ip = context.HttpContext.Request.Headers["X-Real-IP"].FirstOrDefault();
+                Console.WriteLine("请求日志记录异常" + DateTime.Now.ToString() + ex.Message);
             }
-            else if (context.HttpContext.Request.Headers["HTTP_X_FORWARDED_FOR"].Any())
-            {
-                ip = context.HttpContext.Request.Headers["HTTP_X_FORWARDED_FOR"].FirstOrDefault();
-            }
-            else
-            {
-                ip = context.HttpContext.Connection.RemoteIpAddress.ToString();
-            }
-            string user = Guid.Empty.ToString();
-            //if (context.HttpContext.User.Claims.Any())
-            //{
-            //    user = context.HttpContext.User.Claims.First().Value;
-            //}
-            if(context.HttpContext.Request.Cookies.Where(u=>u.Key=="userId").Any())
-            {
-                user = Utils.FromBase64Str(context.HttpContext.Request.Cookies["userId"]);
-            }
-            string method = context.HttpContext.Request.Method;
-            string url = context.HttpContext.Request.Path.Value;
-            string param = Newtonsoft.Json.JsonConvert.SerializeObject(context.ActionArguments);
-            string remark = context.HttpContext.Request.Headers["User-Agent"];
             
-            string brower = "UnKnown";
-            if (remark.Contains("FireFox"))
-                brower = "FireFox";
-            else if (remark.Contains("Chrome"))
-                brower = "Chrome";
-            else if (remark.Contains("Safari"))
-                brower = "Safari";
-            string logMode = ConfigurationHelper.GetSectionValue("LogMode");
-            if (logMode == "es")
-                Task.Run(() => Logger.writeLogToRedis($"{DateTime.Now.ToString("HH:mm:ss")} {user} {method} {url} \"{param}\" {ip} {brower} \"{remark}\"", "info"));
-            else
-            {
-                string msg = $"{DateTime.Now.ToString("HH:mm:ss")} {user} {method} {url} \"{param}\" {ip} {brower} \"{remark}\"";
-                Logger.Verbose(msg);
-            }
         }
 
         private void ResponseLog(ActionExecutedContext context)
--- Magic.Guangdong.Exam.Client/wwwroot/web/xxtThemes/xxt.js
+++ Magic.Guangdong.Exam.Client/wwwroot/web/xxtThemes/xxt.js
@@ -1,12 +1,15 @@
 ﻿checkLoginStatus();
 
-function xxtLogout() {
+function xxtLogout(loginRedicretUrl) {
+    
     deleteCookie('accountId');
     deleteCookie('accountName');
     deleteCookie('idToken');
     //location.href = '/account/Logout?idToken=' + idToken + '&redirectUrl=' + location.protocol + '//' + location.host + '/account/me';
     location.href = `/account/Logout?idToken=${idToken}&redirectUrl=${location.protocol}//${location.host}/account/me`;
 
+    if (redirecturl)
+        setCooie('loginRedicretUrl', btoa(loginRedicretUrl), 0.25);
 }
 //if (accountId)
 //    $('#examHistoryHref').attr('href', '/exam/history?accountId=' + accountId);
@@ -52,11 +55,82 @@ function changeTheme(theme) {
 
 
 //添加菜单点击时间
-function headerTopMenuEvent() {
+function headerTopMenuEvent(topMenuSelectedImg,topMenuBackgroundImg) {
+    if (!topMenuSelectedImg)
+        topMenuSelectedImg = '/img/menuSelect.png';
+    if (!topMenuBackgroundImg)
+        topMenuBackgroundImg = '/img/menuBg.jpg';
     $('#xxtMenu').on('mouseover', 'li', function () {
-        $(this).css('background', 'url(/img/menuSelect.png)'); // 修改背景色
+        $(this).css('background', 'url(' + topMenuSelectedImg +')'); // 修改背景色
 
     }).on('mouseout', 'li', function () {
         $(this).css('background', ''); // 恢复默认背景色
     });
+
+    
+    $('.xxtNavTop').css('background', 'url(' + topMenuBackgroundImg + ')')
 }
+
+// 封装的通知显示函数
+//function cycleNotices(noticeList, elementId, interval = 3000) {
+//    // 获取元素
+//    const element = document.getElementById(elementId);
+
+//    // 当前索引
+//    let currentIndex = 0;
+
+//    // 更新链接和文本的函数
+//    function updateNotice() {
+//        const currentNotice = noticeList[currentIndex];
+//        element.href = currentNotice.url;
+//        element.textContent = currentNotice.title;
+
+//        // 更新索引，循环回第一个通知
+//        currentIndex = (currentIndex + 1) % noticeList.length;
+//    }
+
+//    // 初始化显示第一个通知
+//    updateNotice();
+
+//    // 设置定时器，每interval毫秒切换一次通知
+//    setInterval(updateNotice, interval);
+//}
+function cycleNotices(noticeList, elementId, interval = 3000) {
+    // 获取元素
+    const element = document.getElementById(elementId);
+
+    // 当前索引
+    let currentIndex = 0;
+
+    // 更新链接和文本的函数
+    function updateNotice() {
+        // 先隐藏当前通知
+        element.classList.remove('visible');
+
+        // 等待一段时间让通知完全隐藏
+        setTimeout(() => {
+            const currentNotice = noticeList[currentIndex];
+            element.href = currentNotice.url;
+            element.textContent = currentNotice.title;
+            if (element.textContent.length > 25) {
+                element.textContent = element.textContent.substring(0, 24) + '...'
+            }
+            element.title = currentNotice.title;
+            if (currentNotice.time) {
+                element.title = currentNotice.title + '，发布时间：' + currentNotice.time;
+            }
+            // 显示新的通知
+            element.classList.add('visible');
+
+            // 更新索引，循环回第一个通知
+            currentIndex = (currentIndex + 1) % noticeList.length;
+        }, 500); // 这个值应该与 CSS 中的 transition 时间一致
+    }
+
+    // 初始化显示第一个通知
+    updateNotice();
+
+    // 设置定时器，每interval毫秒切换一次通知
+    setInterval(updateNotice, interval);
+}
+
