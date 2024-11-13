7b948aa7d3b55c9803870466a9d90928ebe4b6b0
Tony_df <wtlemon@126.com>
2024-11-13 10:15:08 +0800

    修改reportAttribute

--- Magic.Guangdong.DbServices/Entities/ReportAttribute.cs
+++ Magic.Guangdong.DbServices/Entities/ReportAttribute.cs
@@ -1,46 +1,42 @@
-﻿using FreeSql.DatabaseModel;using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using FreeSql.DataAnnotations;
 using Newtonsoft.Json;
-using FreeSql.DataAnnotations;
+using Yitter.IdGenerator;
 
-namespace Magic.Guangdong.DbServices.Entities {
+namespace Magic.Guangdong.DbServices.Entities
+{
 
-	[JsonObject(MemberSerialization.OptIn), Table(DisableSyncStructure = true)]
+    [JsonObject(MemberSerialization.OptIn), Table(DisableSyncStructure = true)]
 	public partial class ReportAttribute {
 
 		[JsonProperty, Column(IsPrimary = true)]
-		public long Id { get; set; }
+		public long Id { get; set; } = YitIdHelper.NextId();
 
-		[JsonProperty]
-		public long ActivityId { get; set; }
-
-		[JsonProperty, Column(DbType = "varchar(4000)")]
-		public string DataFilterJson { get; set; }
-
-		[JsonProperty, Column(DbType = "varchar(50)")]
-		public string DataSource { get; set; }
-
-		[JsonProperty]
-		public Guid? FileId { get; set; }
+		[JsonProperty, Column(InsertValueSql = "getdate()")]
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
 
 		[JsonProperty]
 		public int IsDeleted { get; set; } = 0;
 
-		[JsonProperty, Column(StringLength = 500)]
-		public string Limit { get; set; }
+		[JsonProperty, Column(StringLength = 1000)]
+		public string Other { get; set; }
+
+		[JsonProperty, Column(StringLength = 200, IsNullable = false)]
+		public string Participants { get; set; }
 
 		[JsonProperty, Column(StringLength = 100, IsNullable = false)]
-		public string Name { get; set; }
+		public string ProjectNo { get; set; }
 
-		[JsonProperty, Column(StringLength = 50, IsNullable = false)]
-		public string ShortName { get; set; }
+		[JsonProperty]
+		public long ReportId { get; set; }
 
-		[JsonProperty, Column(DbType = "varchar(50)")]
-		public string Type { get; set; }
+		[JsonProperty, Column(StringLength = 200)]
+		public string School { get; set; }
+
+		[JsonProperty, Column(StringLength = 100)]
+		public string Teacher { get; set; }
+
+		[JsonProperty, Column(StringLength = 200, IsNullable = false)]
+		public string TeamName { get; set; }
 
 	}
 
